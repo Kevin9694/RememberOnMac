d90b1deabebfb95a7808e69762e74ba67d278e86
Kevin_Feng <Kevin_Feng@139.com>
2017-04-14 22:25:21 +0800

    init

---
+++ Remember/Remember/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Remember
+//
+//  Created by Kevin_Feng on 2017/4/14.
+//  Copyright © 2017年 Kevin_Feng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
